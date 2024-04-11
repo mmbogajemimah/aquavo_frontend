1d251d66b18b884cd5ef1153b0c757e0f537241b
Jemimah Mmboga <mmbogajemimah@gmail.com>
2024-04-11 17:00:37 +0300

    Dashboard

---
+++ src/Dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import DashboardPage from './Components/DashboardPage/DashboardPage';
+import RegNavBar from './Components/RegistrationPage/RegNavBar';
+
+function Dashboard() {
+  return (
+    <div>
+      <RegNavBar />
+      <DashboardPage />
+    </div>
+  )
+}
+
+export default Dashboard;
