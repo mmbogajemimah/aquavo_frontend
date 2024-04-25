c3e3d78481815ef62d63b65a22fd07edb5c931e3
Jemimah Mmboga <mmbogajemimah@gmail.com>
2024-04-25 16:11:48 +0300

    Add Select customer username

--- src/Components/Refills/CreateRefillPage.js
+++ src/Components/Refills/CreateRefillPage.js
@@ -1,11 +1,27 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import '../../styles/refills/createrefill.css'
+import { useNavigate } from 'react-router-dom'
 
 function CreateRefillPage() {
   const [customerUsername, setCustomerUsername] = useState('');
   const [amountLiters, setAmountLiters] = useState('');
   const [amountMoney, setAmountMoney] = useState('');
+  const [users, setUsers] = useState([]);
 
+  const navigate = useNavigate();
+
+  useEffect (() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await axios.get('http://127.0.0.1:8000/api/accounts/all_users/');
+        setUsers(response.data);
+      } catch (error) {
+        console.log('Error fetching users:', error);
+      }
+    }
+    fetchUsers();
+  }, []);
 
   // Function to handle Form submission
   const handleSubmit = async (e) => {
@@ -27,18 +43,26 @@ function CreateRefillPage() {
 
       // Handle success Response
       console.log('Refill Created:', response.data)
+
+      // Navigate back to /refills/ route
+      navigate('/refills/')
     } catch (error) {
       console.log('Error creating Refill:', error);
     }
   };
 
   return (
-    <div>
-      <h2>Create Refill</h2>
-      <form onSubmit={handleSubmit}>
+    <div className='create-refill-container'>
+      <h2 className='create-refill-header'>Create Refill</h2>
+      <form className='create-refill-form' onSubmit={handleSubmit}>
         <div>
-          <label htmlFor='customerUsername'>Customer Username:</label>
-          <input type='text' id='customerUsername' value={customerUsername} onChange={(e) => setCustomerUsername(e.target.value)}/>
+        <label htmlFor='customerUsername'>Customer Username:</label>
+          <select id='customerUsername' value={customerUsername} onChange={(e) => setCustomerUsername(e.target.value)}>
+            <option value="">Select a user</option>
+            {users.map(user => (
+              <option key={user.id} value={user.username}>{user.username}</option>
+            ))}
+          </select>
         </div>
 
         <div>
