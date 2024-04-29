27e13a9b221be4db318883093f31f5db0c88761b
Jemimah Mmboga <mmbogajemimah@gmail.com>
2024-04-29 18:45:04 +0300

    Dashboard with User Analytics

--- src/Components/DashboardPage/DashboardPage.js
+++ src/Components/DashboardPage/DashboardPage.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../../styles/dashboard/dashboard-page.css';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+import GraphComponent from './GraphComponent';
 
 function DashboardPage() {
+  const [totalUsers, setTotalUsers] = useState(0);
+
+  useEffect(() => {
+    fetchTotalUsers();
+  }, []);
+
+  const fetchTotalUsers = async () => {
+    try {
+      const response = await axios.get('http://127.0.0.1:8000/api/accounts/total_users/'); // Use Axios to make a GET request
+      // console.log(response.data)
+      setTotalUsers(response.data); // Set totalUsers state with received data
+    } catch (error) {
+      console.error('Error fetching total users:', error);
+    }
+  };
+
   return (
     <div className='dashboard'>
       <div className='separator-line'></div>
@@ -14,7 +32,15 @@ function DashboardPage() {
       </div>
 
       <div className='content'>
-        <h1>Welcome to the Dashboard</h1>
+        <h1 className='dashboard-header'>Aquavo Dashboard</h1>
+        <h2 className='user-analytics-header'> User Analytics</h2>
+        <div className='user-stats'>
+          <div className='total-users-card'>
+            <h3>Total Users</h3>
+            <p>{totalUsers}</p>
+          </div>
+          <GraphComponent />
+        </div>
       </div>
     </div>
   )
