96c46062c57142f8fb0fa90a41c15bca60bf8deb
Jemimah Mmboga <mmbogajemimah@gmail.com>
2024-04-30 19:36:59 +0300

    GetAllRefillsPage

--- src/Components/Refills/GetAllRefillsPage.js
+++ src/Components/Refills/GetAllRefillsPage.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Link, useParams, useNavigate } from 'react-router-dom';
 import '../../styles/refills/getallrefills.css';
+import RefillsNavBar from './RefillsNavBar';
 
 function GetAllRefillsPage() {
   // State variables for refills data and loading/error status
@@ -42,41 +43,45 @@ function GetAllRefillsPage() {
   };
 
   return (
-    <div className='container'>
-      <h2 className='header'>All Refills</h2>
-      <table className='table' >
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Vendor Username</th>
-                <th>Customer Username</th>
-                <th>Amount Liters</th>
-                <th>Amount Money</th>
-                <th>Refill date</th>
-                <th>Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            {refills.map((refill, index) => (
-                <tr key={refill.id}>
-                    <td>{index + 1}</td>
-                    <td>{refill.customer.username}</td>
-                    <td></td>
-                    <td>{refill.amount_liters}</td>
-                    <td>{refill.amount_money}</td>
-                    <td>{formatDate(refill.created_at)}</td>
-                    <td className='action-buttons'>
-                        <Link to={`/update_refill/${refill.id}/`} className='update-action-button'>View</Link>
-                    </td>
-                </tr>
-            ))}
-        </tbody>
-      </table>
-      <div className='create-button-div'>
-        <Link to={`/create_refill/`} className='create-refill-button' >Create Refill</Link>
+    <>
+      <RefillsNavBar />
+      <div className='container'>
+        <h2 className='header'>All Refills</h2>
+        <Link className='refills-back-button' to={'/dashboard'}>Back</Link>
+        <table className='table' >
+          <thead>
+              <tr>
+                  <th>ID</th>
+                  <th>Vendor Username</th>
+                  <th>Customer Username</th>
+                  <th>Amount Liters</th>
+                  <th>Amount Money</th>
+                  <th>Refill date</th>
+                  <th>Actions</th>
+              </tr>
+          </thead>
+          <tbody>
+              {refills.map((refill, index) => (
+                  <tr key={refill.id}>
+                      <td>{index + 1}</td>
+                      <td>{refill.customer.username}</td>
+                      <td>{refill.customer_refilling}</td>
+                      <td>{refill.amount_liters}</td>
+                      <td>{refill.amount_money}</td>
+                      <td>{formatDate(refill.created_at)}</td>
+                      <td className='action-buttons'>
+                          <Link to={`/update_refill/${refill.id}/`} className='update-action-button'>View</Link>
+                      </td>
+                  </tr>
+              ))}
+          </tbody>
+        </table>
+        <div className='create-button-div'>
+          <Link to={`/create_refill/`} className='create-refill-button' >Create Refill</Link>
+        </div>
+        
       </div>
-      
-    </div>
+    </>
   )
 }
 
